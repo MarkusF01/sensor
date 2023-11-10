347d329e4f284c771d7911839cfb1f4cf0686b1b
Markus Fehringer <2210859004@fh-burgenland.at>
2023-11-10 18:59:55 +0100

    add missing gateway package, add basic service registry, no client to connect to yet

---
+++ service_registry/src/test/java/at/fehringer_reihs/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fehringer_reihs.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
